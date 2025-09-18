753db973e0f41124794395a6025d08251d71d1d3
SLU-marten <75778930+SLU-marten@users.noreply.github.com>
2025-09-19 00:48:48 +0200

    Använder adapter-auto för Vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,10 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from '@sveltejs/adapter-static'; // <-- LÄGG TILL DENNA RAD
+import adapter from '@sveltejs/adapter-auto'; // Byt tillbaka till adapter-auto
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({ // <-- LÄGG TILL `kit`-OBJEKTET
-			fallback: 'index.html' // Viktigt för single-page apps
-		})
+		adapter: adapter() // Använd den utan extra inställningar
 	},
 	preprocess: vitePreprocess()
 };
